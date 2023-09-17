6167e1aabdf5cd326c1df231e24b3091550caf74
RavichandraYellenki <134284687+RavichandraYellenki@users.noreply.github.com>
2023-09-17 13:02:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ btnElement.addEventListener("click",swapTheme);
 function swapTheme(){
 	let element = document.getElementById("app");
 	app.classList.toggle("night");
+	let element2 = document.getElementById("swap");
+	element2.classList.toggle("button_night");
 }
